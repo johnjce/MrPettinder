ff0eaeb20f0c79a55c6e4a50df079efa9e4b8e38
johnjce <jjcinformatik@gmail.com>
2018-11-21 02:55:02 +0000

    peticion con cabeceras enviada en subforum, responde error 401

--- src/providers/apiurl/apiurl.ts
+++ src/providers/apiurl/apiurl.ts
@@ -1,18 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the ApiurlProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ApiurlProvider {
 
   constructor(public http: HttpClient) {}
   getAPIURL(){
-    return "http://3d2208cc.ngrok.io";
+    return "http://0876f473.ngrok.io";
   }
 
 }
--- src/providers/subforums/subforums.ts
+++ src/providers/subforums/subforums.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs';
@@ -10,6 +10,7 @@ export class SubforumsProvidersProvider {
   constructor(public http: HttpClient, private apiurlProvider: ApiurlProvider) {
   }
   list(): Observable< any > {
-    return this.http.get(this.apiurlProvider.getAPIURL()+'/subforums/');
+    return this.http.get(this.apiurlProvider.getAPIURL()+'/subforums',{ headers: new HttpHeaders().set('Content-Type', 'application/json').set('Authorization', 'Basic dXNlcjpwYXNzd29yZA==')})
+    .map((res: Response) => res.json());
   }
 }
