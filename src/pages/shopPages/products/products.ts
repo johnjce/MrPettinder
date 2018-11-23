b42b2e804b11604cde4900dddbf6c1d232bf6110
johnjce <jjcinformatik@gmail.com>
2018-11-23 12:53:59 +0000

    shop details fixed

--- src/pages/shopPages/products/products.ts
+++ src/pages/shopPages/products/products.ts
@@ -15,9 +15,30 @@ export class ProductsPage {
 
   constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
     this.productList = [
-      {name: "iPhone 5s", price: "799", description: "Gold, 16Gb", imageURL: "https://i.imgur.com/5JHgy9v.jpg"},
-      {name: "Nike T-Shirt", price: "39.90", description: "size L Black", imageURL: "https://i.imgur.com/nHW7Voj.jpg"},
-      {name: "Adidas Shoes", price: "29.00", description: "size 40, White", imageURL: "https://i.imgur.com/d5KNrDz.jpg"}
+      {
+        name: "collar cobaya", 
+        price: "18.20", 
+        description: "size xS, 4cm de largo", 
+        imageURL: "https://petuluku.es/media/catalog/product/cache/1/image/650x650/9df78eab33525d08d6e5fb8d27136e95/a/r/arnes_cobaya_rojo_1.png"
+      },
+      {
+        name: "jaula conejo", 
+        price: "39.90", 
+        description: "size L Black, orange and green", 
+        imageURL: "https://d2rp9bqx0m7ihv.cloudfront.net/media/1/photos/products/115998/82734-001536n_1_g.jpg"
+      },
+      {
+        name: "huesos de jamon", 
+        price: "29.00", 
+        description: "40, size middle", 
+        imageURL: "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201607/13/00149510203844____1__640x640.jpg"
+      },
+      {
+        name: "TecTake game tree", 
+        price: "110.60", 
+        description: "With this extravagant TecTake game tree, the kittens of the house can enjoy themselves as dwarfs. Adventures and fun without limits thanks to its multiple levels that offer tubes, camitas, platforms to make you curious, etc.", 
+        imageURL: "https://cdn.tectake.es/media/catalog/product/cache/1/small_image/1472x/0dc2d03fe217f8c83829496872af24a0/4/0/402742-4.jpg"
+      }
     ];
   }
 
